be45d2cd91d8825d52b3ecc7ed03bd0f13ddf7df
mafanhe <827149941@qq.com>
2016-03-28 23:22:09 +0800

    add files

--- EASY/ASCIIArt.py
+++ EASY/ASCIIArt.py
@@ -1,7 +1,6 @@
 import sys
 import math
 
-#没有过最后一个检查点
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
@@ -13,17 +12,19 @@ for i in range(h):
     row = input()
     rows.append(list(row))
 
-print (t, file=sys.stderr)
+print(t,l,h, file=sys.stderr)
 
 for i in range(h):
     for x in t:
         if 65<=ord(x.upper())<=90:
-            num=(ord(x.upper())-65)*4
-            for j in range(num,num+4):
+            num=(ord(x.upper())-65)*l
+            for j in range(num,num+l):
                 print (rows[i][j],end="")
         else:
-            for j in range(26*4,27*4):
+            for j in range(26*l,27*l):
                 print (rows[i][j],end="")
     print("")
+
+
 
 
